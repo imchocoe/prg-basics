13966818cd78489da6c8a4fdfff34ad4407f02dc
Emilia Delimata <delimataemilia@gmail.com>
2025-01-05 15:59:42 +0100

    practice makes perfect left

--- 12-FunctionalProgramming/5-2.py
+++ 12-FunctionalProgramming/5-2.py
@@ -1,4 +1,6 @@
 from functools import reduce
-
+def add(x,y):
+    return x+y
 numbers = [2,4,6,3,7,5]
-add=sum(list(filter(lambda a:a%==0,numbers)))
+wyn=reduce(add,filter(lambda a:a%2 ==0,numbers))
+print(wyn)
